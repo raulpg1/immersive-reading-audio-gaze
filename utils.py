78164ed3204c539bdf75757baafbcf9accda4869
raulpg1 <raulpeinado10@gmail.com>
2025-06-25 19:48:59 +0200

    inprove model quality

--- utils.py
+++ utils.py
@@ -11,25 +11,34 @@ import shutil
 import subprocess
 from diffusers import AudioLDMPipeline
 
-from config import AUDIO_OUTPUT_PATH, AUDIO_LDM_MODEL
+from config import AUDIO_OUTPUT_PATH, AUDIO_LDM_MODEL, AUDIO_NUM_INFERENCE_STEPS, AUDIO_GUIDANCE_SCALE
+
 def generate_audioldm(prompt_struct):
 
     shutil.rmtree(AUDIO_OUTPUT_PATH)
     os.makedirs(AUDIO_OUTPUT_PATH)
 
-    pipe = AudioLDMPipeline.from_pretrained(AUDIO_LDM_MODEL, torch_dtype=torch.float16)
+    pipe = AudioLDMPipeline.from_pretrained(AUDIO_LDM_MODEL, torch_dtype=torch.float32)
     pipe = pipe.to("cuda")
 
     linea_act = 0
     for i in prompt_struct.keys():
         for j in range(len(prompt_struct[i])-1):
-            prompt = prompt_struct[i][j]["prompt"]
             linea = prompt_struct[i][j]['linea']
-            length = prompt_struct[i][j]["length"]
-            print(f"[AUDIOLDM] Generando audio del parrafo {i}: linea: {linea_act+linea} Prompt: {prompt}")
-            audio = pipe(prompt, num_inference_steps=100, audio_length_in_s=length).audios[0]
+
+            print(f"[AUDIOLDM] Generando audio del parrafo {i}: linea: {linea_act+linea} Prompt: {prompt_struct[i][j]['prompt']}")
+
+            audio = pipe(prompt = prompt_struct[i][j]["prompt"], 
+                         num_inference_steps = AUDIO_NUM_INFERENCE_STEPS, 
+                         audio_length_in_s = prompt_struct[i][j]["length"], 
+                         guidance_scale = AUDIO_GUIDANCE_SCALE
+                         ).audios[0]
+            
             audio_output_file_path = os.path.join(AUDIO_OUTPUT_PATH,f"{linea_act+linea}.wav")
-            scipy.io.wavfile.write(audio_output_file_path, rate=16000, data=audio)
+
+            scipy.io.wavfile.write(audio_output_file_path, 
+                                   rate=16000, 
+                                   data=audio)
         linea_act += prompt_struct[i][-1]
 
 def read_text(file_path):
